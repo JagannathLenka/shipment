643f40053cdf0327361402be2e469ef2fd248799
mitaliswain <JagannathLenka@Jagannaths-MacBook-Pro.local>
2016-01-17 20:34:00 -0800

    added arrary

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -29,7 +29,7 @@ class MainController < ApplicationController
     shipment = ShipmentDetail.new
     shipment.shipment = xml_data.xpath('//shipment/shipment').text
     shipment.shipment_type = xml_data.xpath('//shipment/shipment_type').text
-    shipment.shipment_carrier = xml_data.xpath('//shipment/shipment_carrier').text
+    shipment.shipment_carrier = 'UPS'
 
     shipments << shipment
 
